1087d469eab826c4fb91859ae1cd4bac1c798ba7
hal2332004 <root@hal2332004>
2025-08-25 17:11:09 +0700

    add code

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+<<<<<<< HEAD
   base: '/Porfolio/', // Match your GitHub repository name
+=======
+  base: '/Portfolio/', // Match your GitHub repository name correctly (Portfolio not Porfolio)
+>>>>>>> 90bce7e (add package and vite config)
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
