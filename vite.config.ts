93869de79b53d7b6601a072d26a8c0b7386596e3
Ho An Loc <119057391+hal2332004@users.noreply.github.com>
2025-08-25 17:03:25 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/', // Match your GitHub repository name correctly (Portfolio not Porfolio)
+  // base: '/Portfolio/', // Match your GitHub repository name correctly (Portfolio not Porfolio)
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
